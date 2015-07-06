5a5eee57f4692603dc934653198ab445b6e32dfc
Chien-Liang Fok <liangfok@utexas.edu>
2015-07-06 10:59:01 -0500

    Added an example of how to publish a point cloud of a sphere.

---
+++ nodes/point_cloud_message_creator.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import rospy                        #for interacting with ROS topics and parameters
+from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2, PointField
+
+import math
+import random
+import struct
+import ctypes
+
+# The frequency of publishing the PointCloud2 message
+PUBLISH_FREQUENCY = 10
+
+# The following code was copied from:
+# https://code.google.com/p/ros-by-example/source/browse/trunk/rbx_vol_1/rbx1_apps/src/point_cloud2.py?r=125
+
+_DATATYPES = {}
+_DATATYPES[PointField.INT8]    = ('b', 1)
+_DATATYPES[PointField.UINT8]   = ('B', 1)
+_DATATYPES[PointField.INT16]   = ('h', 2)
+_DATATYPES[PointField.UINT16]  = ('H', 2)
+_DATATYPES[PointField.INT32]   = ('i', 4)
+_DATATYPES[PointField.UINT32]  = ('I', 4)
+_DATATYPES[PointField.FLOAT32] = ('f', 4)
+_DATATYPES[PointField.FLOAT64] = ('d', 8)
+
+def _get_struct_fmt(is_bigendian, fields, field_names=None):
+    fmt = '>' if is_bigendian else '<'
+
+    offset = 0
+    for field in (f for f in sorted(fields, key=lambda f: f.offset) if field_names is None or f.name in field_names):
+        if offset < field.offset:
+            fmt += 'x' * (field.offset - offset)
+            offset = field.offset
+        if field.datatype not in _DATATYPES:
+            print >> sys.stderr, 'Skipping unknown PointField datatype [%d]' % field.datatype
+        else:
+            datatype_fmt, datatype_length = _DATATYPES[field.datatype]
+            fmt    += field.count * datatype_fmt
+            offset += field.count * datatype_length
+
+    return fmt
+
+def create_cloud(header, fields, points):
+    """
+    Create a L{sensor_msgs.msg.PointCloud2} message.
+
+    @param header: The point cloud header.
+    @type  header: L{std_msgs.msg.Header}
+    @param fields: The point cloud fields.
+    @type  fields: iterable of L{sensor_msgs.msg.PointField}
+    @param points: The point cloud points.
+    @type  points: list of iterables, i.e. one iterable for each point, with the
+                   elements of each iterable being the values of the fields for
+                   that point (in the same order as the fields parameter)
+    @return: The point cloud.
+    @rtype:  L{sensor_msgs.msg.PointCloud2}
+    """
+
+    cloud_struct = struct.Struct(_get_struct_fmt(False, fields))
+
+    buff = ctypes.create_string_buffer(cloud_struct.size * len(points))
+
+    point_step, pack_into = cloud_struct.size, cloud_struct.pack_into
+    offset = 0
+    for p in points:
+        pack_into(buff, offset, *p)
+        offset += point_step
+
+    return PointCloud2(header=header,
+                       height=1,
+                       width=len(points),
+                       is_dense=False,
+                       is_bigendian=False,
+                       fields=fields,
+                       point_step=cloud_struct.size,
+                       row_step=cloud_struct.size * len(points),
+                       data=buff.raw)
+
+def create_cloud_xyz32(header, points):
+    """
+    Create a L{sensor_msgs.msg.PointCloud2} message with 3 float32 fields (x, y, z).
+
+    @param header: The point cloud header.
+    @type  header: L{std_msgs.msg.Header}
+    @param points: The point cloud points.
+    @type  points: iterable
+    @return: The point cloud.
+    @rtype:  L{sensor_msgs.msg.PointCloud2}
+    """
+    fields = [PointField('x', 0, PointField.FLOAT32, 1),
+              PointField('y', 4, PointField.FLOAT32, 1),
+              PointField('z', 8, PointField.FLOAT32, 1)]
+    return create_cloud(header, fields, points)
---
+++ nodes/point_cloud_publish_example.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import rospy                        #for interacting with ROS topics and parameters
+from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2, PointField
+
+import math
+import random
+import struct
+import ctypes
+
+import point_cloud_message_creator
+
+# The frequency of publishing the PointCloud2 message
+PUBLISH_FREQUENCY = 10
+
+def genRandom(min, max):
+    delta = abs(max - min)
+    rand = random.random()
+    return min + (rand * delta)
+
+# Generates a random point on a sphere
+# See: http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
+def genRandomPointOnSphere(radius):
+    phi = genRandom(0, 2 * math.pi)
+    costheta = genRandom(-1, 1)
+    u = genRandom(0, 1)
+
+    theta = math.acos(costheta)
+    r = radius * (u ** (1 / 3.0))
+
+    # theta is angle relative to Z axis
+    # phi is angle relative to X axis
+
+    x = r * math.sin(theta) * math.cos(phi)
+    y = r * math.sin(theta) * math.sin(phi)
+    z = r * math.cos(theta)
+
+    result = []
+    result.append(x)
+    result.append(y)
+    result.append(z)
+
+    return result
+
+if __name__ == "__main__":
+
+    # Initialize the ROS node
+    rospy.init_node('point_cloud_example_sphere', anonymous=True)
+
+    # Create a rate control object
+    rate = rospy.Rate(PUBLISH_FREQUENCY)
+
+    while not rospy.is_shutdown():
+
+        # Instantiate a 'points' array and fill it up with random points on a sphere
+        points = []
+
+        for x in range(0, 10000):
+            currPoint = genRandomPointOnSphere(1.0)
+            points.append(currPoint)
+
+        # Instantiate a header
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = "world"
+
+        # Create a message of type sensor_msgs.PointCloud2
+        pointCloud = point_cloud_message_creator.create_cloud_xyz32(header, points)
+
+        # Instantiate a publisher for the sensor_msgs.PointCloud2 message
+        cloudPublisher = rospy.Publisher("pointCloud", PointCloud2, queue_size = 0)
+
+        cloudPublisher.publish(pointCloud)
+        rate.sleep()
+
+    print "Sphere point cloud example publisher done. Waiting until ctrl+c is hit..."
+    rospy.spin()  # just to prevent this node from exiting
